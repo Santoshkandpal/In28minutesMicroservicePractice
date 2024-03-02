a69a2173a315c2407cbafdf0711bbd77101513d6
santoshkandpal09 <156089365+santoshkandpal09@users.noreply.github.com>
2024-03-02 17:37:32 +0530

    First update of microservices

---
+++ H2DatabaseSetup/src/main/java/com/in28minutes/springboot/firstrestapi/H2DatabaseSetupApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.firstrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DatabaseSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DatabaseSetupApplication.class, args);
+	}
+
+}
